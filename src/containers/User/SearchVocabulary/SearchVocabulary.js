cc6b8fcae481f39e1bdde75392687a7392d9a2ed
Trong Nhan <cnttute21110571@gmail.com>
2024-11-06 21:37:06 +0700

    save vocab to flashcard in search word

--- src/containers/User/SearchVocabulary/SearchVocabulary.js
+++ src/containers/User/SearchVocabulary/SearchVocabulary.js
@@ -6,7 +6,7 @@ import CustomScrollbars from '../../../components/CustomScrollbars';
 import { push } from "connected-react-router";
 import { getSearchVocabularyApi, getAudioVocabularyApi } from '../../../services/vocabularyService';
 import { getAllFlashcards, saveVocabtoFlashcard, deleteVocabFromFlashcard } from '../../../services/flashcardService';
-import './SearchVocabulary.scss'
+import './SearchVocabulary.scss';
 
 class SearchVocabulary extends Component {
     constructor(props) {
@@ -23,7 +23,137 @@ class SearchVocabulary extends Component {
             flashcards: [],
             savedWords: {}
         };
+
     }
+
+    // handleDeleteFromFlashcard = async (flashcardId, vocabularyId) => {
+    //     try {
+    //         const response = await deleteVocabFromFlashcard({ flashcardId, vocabularyId });
+
+    //         // Kiểm tra phản hồi từ API và cập nhật giao diện
+    //         if (response && response.data.errCode === 0) {
+    //             alert("Đã xóa từ khỏi flashcard thành công!");
+    //             this.setState(prevState => ({
+    //                 savedWords: {
+    //                     ...prevState.savedWords,
+    //                     [flashcardId]: false, // Đánh dấu flashcard này chưa được lưu
+    //                 }
+    //             }));
+    //         } else if (response && response.data.errMessage) {
+    //             alert(`Error: ${response.data.errMessage}`);
+    //         } else {
+    //             alert("Đã xảy ra lỗi không xác định.");
+    //         }
+    //     } catch (error) {
+    //         console.error("Lỗi khi xóa từ khỏi flashcard:", error);
+    //         alert(`Error: ${error.response ? error.response.data.errMessage : error.message}`);
+    //     }
+    // };
+
+
+    handleSaveClick = async () => {
+        try {
+            const res = await getAllFlashcards(this.props.userInfor.id);
+            if (res.errCode === 0) {
+                this.setState({
+                    flashcards: res.flashcards,
+                    showModal: true
+                });
+            }
+
+        } catch (error) {
+            console.error("Lỗi khi tải flashcards", error);
+        }
+    };
+
+
+    handleSaveToFlashcard = async (flashcardId) => {
+        const { word, result, audioUS, audioUK } = this.state;
+
+        // Lấy chuỗi HTML từ API trả về
+        const documentHTML = result?.tratu?.[0]?.fields?.fulltext;
+        if (!documentHTML) {
+            console.log("Không có tài liệu HTML để xử lý.");
+            return;
+        }
+
+        try {
+
+            // Sử dụng DOMParser để chuyển đổi HTML thành tài liệu DOM
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(documentHTML, "text/html");
+
+            // Trích xuất các giá trị cần thiết từ tài liệu DOM
+            let pronunciation = '';
+            let partOfSpeech = '';
+            let definition = '';
+            let exampleSentence = '';
+
+            // Tìm phần tử chứa cách phát âm (pronunciation)
+            const pronunciationElement = doc.querySelector('#C_C font[color="#9e9e9e"]');
+            if (pronunciationElement) {
+                pronunciation = pronunciationElement.innerText;
+            }
+
+            // Tìm phần tử chứa loại từ (partOfSpeech)
+            const partOfSpeechElement = doc.querySelector('#tl b font[color="#1a76bf"]');
+            if (partOfSpeechElement) {
+                partOfSpeech = partOfSpeechElement.innerText;
+            }
+
+            // Tìm phần tử chứa định nghĩa (definition)
+            const definitionElement = doc.querySelectorAll('#mn #C_C')[0];
+            if (definitionElement) {
+                definition = definitionElement.innerText;
+            }
+
+            // Tìm phần tử chứa ví dụ (exampleSentence)
+            const exampleSentenceElement = doc.querySelectorAll('#mh_n #C_C font[color="#7E7E7E"]')[0];
+            if (exampleSentenceElement) {
+                exampleSentence = exampleSentenceElement.innerText;
+            }
+
+            // In ra các giá trị để kiểm tra
+            console.log('Pronunciation:', pronunciation);
+            console.log('Part of Speech:', partOfSpeech);
+            console.log('Definition:', definition);
+            console.log('Example Sentence:', exampleSentence);
+
+            // Gửi dữ liệu từ vựng đến API
+            const response = await saveVocabtoFlashcard({
+                word,
+                definition,
+                partOfSpeech,
+                exampleSentence,
+                pronunciation,
+                audioUS,
+                audioUK,
+                flashcardId
+            });
+
+            // Kiểm tra phản hồi từ API và hiển thị thông báo
+            if (response && response.errCode === 0) {
+                alert("Đã lưu từ vào flashcard thành công!");
+                this.setState(prevState => ({
+                    showModal: false,
+                    savedWords: {
+                        ...prevState.savedWords,
+                        [flashcardId]: true, // Đánh dấu flashcard này đã được lưu
+                    }
+                }));
+            }
+
+            else if (response && response.errMessage) {
+                alert(`Error: ${response.errMessage}`);
+            } else {
+                alert("Đã xảy ra lỗi không xác định.");
+            }
+        } catch (error) {
+            console.error("Lỗi khi lưu từ vào flashcard:", error);
+            alert(`Error: ${error.response ? error.response.data.errMessage : error.message}`);
+        }
+
+    };
 
     handleInputChange = (event) => {
         this.setState({
@@ -50,12 +180,8 @@ class SearchVocabulary extends Component {
         );
     };
 
-    handleSearchTypeChange = (type) => {
-        this.setState({ searchType: type }, this.resetSearch);
-    };
-
     handleSearch = async () => {
-        const { word, lang, searchType } = this.state;
+        const { word, lang } = this.state;
 
         if (!word) {
             this.setState({ error: 'Please enter a word to search' });
@@ -63,20 +189,14 @@ class SearchVocabulary extends Component {
         }
 
         try {
-            let response = {};
-            let dataAudio = {};
-            // lấy nội dung của từ
-            response = await getSearchVocabularyApi(word, lang);
-
-            // lấy file âm thanh của từ
-            dataAudio = await getAudioVocabularyApi(word);
+            let response = await getSearchVocabularyApi(word, lang);
+            let dataAudio = await getAudioVocabularyApi(word);
             this.setState({
                 result: response,
                 audioUS: dataAudio.audioUS,
                 audioUK: dataAudio.audioUK,
                 error: ''
             });
-            console.log("check res: ", this.state.result)
         } catch (error) {
             this.setState({ error: 'Word not found! Plz enter another word!' });
         }
@@ -92,8 +212,8 @@ class SearchVocabulary extends Component {
     };
 
     render() {
-        const { word, result, error, searchType, audioUS, audioUK } = this.state;
-
+        const { word, result, error, audioUS, audioUK, showModal, flashcards } = this.state;
+        console.log("KT flashcard: ", flashcards);
         return (
             <React.Fragment>
                 <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
@@ -108,18 +228,24 @@ class SearchVocabulary extends Component {
                                 <input
                                     type='text'
                                     value={word}
-                                    onChange={(event) => { this.handleInputChange(event) }}
+                                    onChange={this.handleInputChange}
                                     placeholder='Enter a word'
                                 />
-                                <button onClick={() => { this.handleSearch() }}>Tra từ</button>
+                                <button onClick={this.handleSearch}>Tra từ</button>
                             </div>
                             {error && <div className='search-vocabulary-input-error'>😊{error}😊</div>}
 
                             <div className='search-vocabulary-content'>
-                                {result && result.tratu && result.tratu.length > 0 && searchType === 'dictionary' && (
+                                {result && result.tratu && result.tratu.length > 0 && (
                                     <div className='vocabulary-result'>
                                         <div className='vocabulary-header'>
                                             <h3>Kết quả cho "{word}"</h3>
+                                            {/* Thêm nút Save */}
+
+                                            <i
+                                                className='fa-solid fa-heart'
+                                                onClick={() => { this.handleSaveClick() }}
+                                            ></i>
                                         </div>
 
                                         <div className='audio-controls'>
@@ -138,30 +264,53 @@ class SearchVocabulary extends Component {
                                         <div className='vocabulary-definition'>
                                             {result.tratu[0].fields.fulltext && this.renderDefinition(result.tratu[0].fields.fulltext)}
                                         </div>
+                                        {/* Modal hiển thị danh sách flashcards */}
+                                        {showModal && (
+                                            <div className="flashcard-modal">
+                                                <h2>Chọn Flashcard để lưu</h2>
+                                                <ul>
+                                                    {Array.isArray(flashcards) && flashcards.length > 0 ? (
+                                                        flashcards.map(flashcard => (
+                                                            <li key={flashcard.id}>
+                                                                <button>
+                                                                    {flashcard.flashcardName}
+                                                                </button>
+                                                                <i
+                                                                    className={`fa-heart ${this.state.savedWords[flashcard.id] ? 'fa-solid' : 'fa-regular'}`}
+                                                                    style={{ color: this.state.savedWords[flashcard.id] ? 'pink' : 'black' }}
+                                                                    onClick={() => { this.handleSaveToFlashcard(flashcard.id) }}
+                                                                ></i>
+
+                                                            </li>
+
+                                                        ))
+                                                    ) : (
+                                                        <li>Không có flashcard nào để hiển thị.</li>
+                                                    )}
+                                                </ul>
+                                                <button onClick={() => this.setState({ showModal: false })}>Đóng</button>
+                                            </div>
+                                        )}
                                     </div>
                                 )}
                             </div>
                         </div>
                     </div>
-                    <HomeFooter />
+
                 </CustomScrollbars>
             </React.Fragment>
         );
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn,
-        userInfor: state.user.userInfor,
-    };
-};
+const mapStateToProps = (state) => ({
+    isLoggedIn: state.user.isLoggedIn,
+    userInfor: state.user.userInfor,
+    exam: state.user.selectedExam,
+});
 
-const mapDispatchToProps = dispatch => {
-    return {
-        navigate: (path) => dispatch(push(path)),
-        // processLogout: () => dispatch(actions.processLogout()),
-    };
-};
+const mapDispatchToProps = (dispatch) => ({
+    navigate: (path) => dispatch(push(path)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchVocabulary);
